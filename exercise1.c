4615850b1c77089819f53423740f3f32ba751006
vlad777442 <vefirst@gmail.com>
2024-06-16 16:09:19 -0400

    added variables

---
+++ exercise1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define NUM_VALUES 250
+
+
+int value[NUM_VALUES];
+
+int main()
+{
+    unsigned int sum = 0;
+    char i = 0;
+    //output some text
+    printf("Hello World\n");
+    return 0;
+}
